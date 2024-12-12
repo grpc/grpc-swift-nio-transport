a7b2f50342ede48b0f1c4d480c4eafc0264786a0
Rick Newton-Rogers <rnro@apple.com>
2024-12-12 10:28:17 +0000

    Enable MemberImportVisibility check on all targets (#45)
    Enable MemberImportVisibility check on all targets. Use a standard
    string header and footer to bracket the new block for ease of updating
    in the future with scripts.
    
    ---------
    
    Co-authored-by: George Barnett <gbarnett@apple.com>

--- Package.swift
+++ Package.swift
@@ -67,6 +67,7 @@ let defaultSwiftSettings: [SwiftSetting] = [
   .swiftLanguageMode(.v6),
   .enableUpcomingFeature("ExistentialAny"),
   .enableUpcomingFeature("InternalImportsByDefault"),
+  .enableUpcomingFeature("MemberImportVisibility"),
 ]
 
 let targets: [Target] = [
