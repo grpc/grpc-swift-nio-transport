aaeba93e58a23257df1f2eebb1470338cc6d078c
Gus Cairo <gus.cairo@apple.com>
2025-01-17 17:38:32 +0000

    Remove workaround for issue #43 (#63)
    This has been resolved in https://github.com/apple/swift-nio/pull/3032
    and the fix is present in NIO since 2.78.0, which this package already
    depends on. Removing workaround in tests.

--- Tests/GRPCNIOTransportHTTP2Tests/HTTP2TransportTests.swift
+++ Tests/GRPCNIOTransportHTTP2Tests/HTTP2TransportTests.swift
@@ -1392,8 +1392,6 @@ final class HTTP2TransportTests: XCTestCase {
       let request = ClientRequest(message: input)
       try await control.serverStream(request: request) { response in
         XCTAssertEqual(Array(response.metadata["echo-scheme"]), ["http"])
-        // Workaround https://github.com/grpc/grpc-swift-nio-transport/issues/43
-        for try await _ in response.messages {}
       }
     }
   }
@@ -1424,8 +1422,6 @@ final class HTTP2TransportTests: XCTestCase {
       }
       try await control.bidiStream(request: request) { response in
         XCTAssertEqual(Array(response.metadata["echo-scheme"]), ["http"])
-        // Workaround https://github.com/grpc/grpc-swift-nio-transport/issues/43
-        for try await _ in response.messages {}
       }
     }
   }
