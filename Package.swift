a4c6c2848f0f0aada9dcb9144f17188a4c2ac655
Gus Cairo <gus.cairo@apple.com>
2025-03-17 11:21:05 +0000

    Update min version of grpc-swift to fix static Linux SDK build

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let products: [Product] = [
 let dependencies: [Package.Dependency] = [
   .package(
     url: "https://github.com/grpc/grpc-swift.git",
-    from: "2.0.0"
+    from: "2.1.1"
   ),
   .package(
     url: "https://github.com/apple/swift-nio.git",
