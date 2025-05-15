fbfd3df478df4711b084a584b398c2699a70d46e
George Barnett <gbarnett@apple.com>
2025-05-15 09:36:11 +0100

    Update generated code
    Motivation:
    
    grpc-swift 2.2.0 added availability annotations to generated code. The
    CI job checking generated code is now failing..
    
    Modifications:
    
    Regenerate
    
    Result:
    
    CI passes

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let products: [Product] = [
 let dependencies: [Package.Dependency] = [
   .package(
     url: "https://github.com/grpc/grpc-swift.git",
-    from: "2.0.0"
+    from: "2.2.0"
   ),
   .package(
     url: "https://github.com/apple/swift-nio.git",
