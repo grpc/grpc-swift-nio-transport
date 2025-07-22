28b8e4a1fa8615d3ce0c7db360e9db7fa272dd74
George Barnett <gbarnett@apple.com>
2025-07-22 16:41:23 +0100

    Avoid deprecation warnings (#119)
    Motivation:
    
    NIOSSL deprecated an init on NIOSSLCertificate. This now warns and
    causes some CI jobs to fail as a result.
    
    Modifications:
    
    - Switch to new API
    
    Result:
    
    No warnings, nightly CI passes

--- Package.swift
+++ Package.swift
@@ -51,7 +51,7 @@ let dependencies: [Package.Dependency] = [
   ),
   .package(
     url: "https://github.com/apple/swift-nio-ssl.git",
-    from: "2.31.0"
+    from: "2.33.0"
   ),
   .package(
     url: "https://github.com/apple/swift-nio-extras.git",
